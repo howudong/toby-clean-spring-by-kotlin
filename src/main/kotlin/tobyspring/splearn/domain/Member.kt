ebdab49721cf62d0ebd4e08c4fab6116ecd5a493
howudong <tjdvy963@naver.com>
2025-06-29 21:06:30 +0900

    feat : 가입 완료 로직 및 테스트 코드 작성

--- src/main/kotlin/tobyspring/splearn/domain/Member.kt
+++ src/main/kotlin/tobyspring/splearn/domain/Member.kt
@@ -1,8 +1,15 @@
 package tobyspring.splearn.domain
 
 class Member(
-    val email : String,
-    val nickname : String,
-    val passwordHash : String,
-    val status: MemberStatus = MemberStatus.PENDING
-)
+    val email: String,
+    val nickname: String,
+    var passwordHash: String,
+) {
+    var status: MemberStatus = MemberStatus.PENDING
+        private set
+
+    fun activate() {
+        check(status == MemberStatus.PENDING) { "PENDING 상태가 아닙니다" }
+        status = MemberStatus.ACTIVE
+    }
+}
--- src/test/kotlin/tobyspring/splearn/domain/MemberTest.kt
+++ src/test/kotlin/tobyspring/splearn/domain/MemberTest.kt
@@ -1,5 +1,6 @@
 package tobyspring.splearn.domain
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
@@ -11,6 +12,20 @@ class MemberTest : BehaviorSpec({
             Then("PENDING으로 생성되어야 한다.") {
                 newMember.status shouldBe MemberStatus.PENDING
 
+            }
+        }
+    }
+    Given("회원이 생성되고,") {
+        val newMember = Member("tjdvy963@naver.com", "howudong", "asdfsfd")
+        When("처음 가입 완료 될때") {
+            newMember.activate()
+            Then("멤버 상태는 ACTIVE 상태가 되어야 한다") {
+                newMember.status shouldBe MemberStatus.ACTIVE
+            }
+        }
+        When("동일 멤버가 두번째로 가입 완료를 시도하면") {
+            Then("IllegalStateException이 발생해야한다.") {
+                shouldThrow<IllegalStateException> { newMember.activate() }
             }
         }
     }

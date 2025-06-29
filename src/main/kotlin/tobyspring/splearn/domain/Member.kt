0280bee8cc30b0b8e2cefc6f3f388fb876d24617
howudong <tjdvy963@naver.com>
2025-06-29 22:17:48 +0900

    refactor : PasswordEncoder를 만들고 Member에 의존성 주입하는 코드로 코드 리팩토링

--- src/main/kotlin/tobyspring/splearn/domain/Member.kt
+++ src/main/kotlin/tobyspring/splearn/domain/Member.kt
@@ -1,10 +1,16 @@
 package tobyspring.splearn.domain
 
-class Member(
+class Member private constructor(
     val email: String,
     val nickname: String,
     var passwordHash: String,
 ) {
+    companion object {
+        fun create(email: String, nickname: String, password: String, passwordEncoder: PasswordEncoder): Member {
+            return Member(email, nickname, passwordEncoder.encode(password))
+        }
+    }
+
     var status: MemberStatus = MemberStatus.PENDING
         private set
 
@@ -16,7 +22,7 @@ class Member(
 
     fun deActivated() {
         check(status == MemberStatus.ACTIVE) { "ACTIVE 상태가 아닙니다" }
-        
+
         status = MemberStatus.DEACTIVATED
     }
 }
--- src/test/kotlin/tobyspring/splearn/domain/MemberTest.kt
+++ src/test/kotlin/tobyspring/splearn/domain/MemberTest.kt
@@ -5,63 +5,82 @@ import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 
 
-class MemberTest : BehaviorSpec({
-    Given("멤버가") {
-        When("처음 생성되면") {
-            val newMember = Member("tjdvy963@naver.com", "howudong", "asdfsfd")
-            Then("PENDING으로 생성되어야 한다.") {
-                newMember.status shouldBe MemberStatus.PENDING
+class MemberTest : BehaviorSpec() {
+    private lateinit var member: Member
 
+    init {
+        beforeContainer { testCase ->
+            if (testCase.descriptor.depth() == 1) {
+                member = Member.create(
+                    "tjdvy953@naver.com",
+                    "howudong",
+                    "secret",
+                    object : PasswordEncoder {
+                        override fun encode(password: String): String = password.uppercase()
+
+                        override fun matches(password: String, passwordHash: String): Boolean =
+                            password.uppercase() == passwordHash
+                    })
+            }
+        }
+
+        Given("멤버가")
+        {
+            When("처음 생성되면") {
+                Then("PENDING으로 생성되어야 한다.") {
+                    member.status shouldBe MemberStatus.PENDING
+
+                }
+            }
+        }
+
+        Given("회원이 생성되고,")
+        {
+            When("처음 가입 완료 될때") {
+                member.activate()
+                Then("멤버 상태는 ACTIVE 상태가 되어야 한다") {
+
+                    member.status shouldBe MemberStatus.ACTIVE
+                }
+            }
+        }
+        Given("회원이 ") {
+            When("동일 멤버가 두번째로 가입 완료를 시도하면") {
+                member.activate()
+                Then("IllegalStateException이 발생해야한다.") {
+                    shouldThrow<IllegalStateException> { member.activate() }
+                }
+            }
+        }
+
+        Given("가입 완료된 회원이 주어지고,")
+        {
+            When("해당 회원이 탈퇴했을 때,") {
+                member.activate() // 원래는 GIVEN이 맞는데 코드 동작을 위해 어쩔 수 없이 WHEN 절로 옮김
+                member.deActivated()
+
+                Then("해당 멤버의 상태는 DEACTIVATED가 되어야 한다.") {
+                    member.status shouldBe MemberStatus.DEACTIVATED
+                }
+            }
+        }
+
+        Given("회원이 주어지고,")
+        {
+            When("가입 완료되지 않은 상태에서 탈퇴하려고 하면") {
+                Then("IllegalStateException이 발생해야 한다") {
+                    shouldThrow<IllegalStateException> { member.deActivated() }
+                }
+            }
+            When("이미 탈퇴한 상태에서 또 탈퇴하려고 하면,") {
+                member.activate()
+                member.deActivated()
+
+                Then("IllegalStateException가 발생해야 한다") {
+                    shouldThrow<IllegalStateException> { member.deActivated() }
+                }
             }
         }
     }
-    Given("회원이 생성되고,") {
-        val newMember = Member("tjdvy963@naver.com", "howudong", "asdfsfd")
+}
 
-        When("처음 가입 완료 될때") {
-            newMember.activate()
-            Then("멤버 상태는 ACTIVE 상태가 되어야 한다") {
-
-                newMember.status shouldBe MemberStatus.ACTIVE
-            }
-        }
-
-        When("동일 멤버가 두번째로 가입 완료를 시도하면") {
-
-            Then("IllegalStateException이 발생해야한다.") {
-                shouldThrow<IllegalStateException> { newMember.activate() }
-            }
-        }
-    }
-
-    Given("가입 완료된 회원이 주어지고,") {
-        val newMember = Member("tjdvy963@naver.com", "howudong", "asdfsfd")
-        newMember.activate()
-
-        When("해당 회원이 탈퇴했을 때,") {
-            newMember.deActivated()
-
-            Then("해당 멤버의 상태는 DEACTIVATED가 되어야 한다.") {
-                newMember.status shouldBe MemberStatus.DEACTIVATED
-            }
-        }
-    }
-
-    Given("회원이 주어지고,") {
-        val newMember = Member("tjdvy963@naver.com", "howudong", "asdfsfd")
-
-        When("가입 완료되지 않은 상태에서 탈퇴하려고 하면") {
-            Then("IllegalStateException이 발생해야 한다") {
-                shouldThrow<IllegalStateException> { newMember.deActivated() }
-            }
-        }
-        When("이미 탈퇴한 상태에서 또 탈퇴하려고 하면,") {
-            newMember.activate()
-            newMember.deActivated()
-
-            Then("IllegalStateException가 발생해야 한다") {
-                shouldThrow<IllegalStateException> { newMember.deActivated() }
-            }
-        }
-    }
-})
